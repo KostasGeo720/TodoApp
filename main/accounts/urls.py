ea71871857458c7ee445b3fdbe4e00a86d8fe1c2
Kostas Georgoulis <georgouliskostas7@gmail.com>
2025-11-07 11:50:12 +0200

    Added Login Logic and pages + placeholder home page

--- main/accounts/urls.py
+++ main/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profile/new/', views.new_profile, name='new_profile'),
+    path('login/', views.login, name='login'),
 ]
--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Profile
 from .forms import NewUserProfileForm
 
@@ -17,3 +18,17 @@ def new_profile(request):
     else:
         form = NewUserProfileForm()
     return render(request, 'accounts/new_profile.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            messages.success(request, 'Logged in successfully!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
